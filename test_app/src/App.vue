eca4d86f9bd38fed73e32b835d3d53d4389abbe3
IhoraDev <ihor.lukachuk.dev@gmail.com>
2020-02-12 14:12:45 +0200

    Edit portfolio

---
+++ test_app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+        <router-view></router-view>
+
+        <img alt="Vue logo" src="./assets/logo.png">
+            <vuescroll>
+                <todo />
+            </vuescroll>
+    </div>
+</template>
+
+<script>
+    import todo from "@/components/todo";
+    import style from "@/components/style.scss";
+    import vuescroll from 'vuescroll';
+
+    export default {
+        name : 'app',
+        components:{
+            todo,
+            style,
+            vuescroll
+        }
+    }
+</script>
